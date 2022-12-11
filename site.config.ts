ec57d60263f88eac8a890d21889d83eafad3f07b
Zhenyuan Qiu <qiuzhenyuan1024@gmail.com>
2022-12-11 19:36:21 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Getting-Started-f46aea4e1f244f7b8d50c2b28a3e4cbc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '振源的提问机器',
+  domain: 'https://garrulous-alyssum-61c.notion.site/Getting-Started-f46aea4e1f244f7b8d50c2b28a3e4cbc',
+  author: '振源',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
